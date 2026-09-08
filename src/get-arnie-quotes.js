8c90cc2e436d48b593f65dee8db901145844efa0
admin <gleidson_moreira@trendmicro.com>
2026-09-08 18:54:54 +1000

    impr: Add missing status and error handling

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -7,11 +7,17 @@ const { httpGet } = require('./mock-http-interface');
 const getArnieQuotes = async (urls) => {
   return Promise.all(
     urls.map( async url => {
-      const result = await httpGet(url);
-      if (result.status === 200){
-        return {"Arnie Quote" : JSON.parse(result.body).message}
-      }else if (result.status === 500){
-        return {"FAILURE" : JSON.parse(result.body).message}
+      try {
+        const result = await httpGet(url);
+        if (result.status === 200){
+          return {"Arnie Quote": JSON.parse(result.body).message};
+        }else if (result.status === 500){
+          return {"FAILURE": JSON.parse(result.body).message};
+        }else{
+          return {"FAILURE": "Unexpected status code"};
+        }
+      } catch ( error ){
+        return {"FAILURE": error.message};
       }
     })
   );
