a53a11016e217a96f9985ed1d85a5e3e27526edc
admin <gleidson_moreira@trendmicro.com>
2026-09-08 18:43:21 +1000

    impr: Implement getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -5,7 +5,16 @@ const { httpGet } = require('./mock-http-interface');
  * @returns {ArnieQuoteResponse}
  */
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
+  return Promise.all(
+    urls.map( async url => {
+      const result = await httpGet(url);
+      if (result.status === 200){
+        return {"Arnie Quote" : JSON.parse(result.body).message}
+      }else if (result.status === 500){
+        return {"FAILURE" : JSON.parse(result.body).message}
+      }
+    })
+  );
 };
 
 module.exports = {
